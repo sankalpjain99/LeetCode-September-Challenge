86c8bf91a9ab46fd225b374857ba9fa4ef05890f
Sankalp Jain <sankalpjain99@gmail.com>
2020-09-01 15:54:07 +0530

    Day 1 Solution

--- Week1/Day1-LargestTimeforGivenDigits.cpp
+++ Week1/Day1-LargestTimeforGivenDigits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    //Function to check if given string is a valid timestamp or not
     bool isOkay(string s){
         int n1 = (int)(s[0]-'0');
         int n2 = (int)(s[1]-'0');
@@ -13,9 +14,11 @@ public:
         return true;
     }
     
+    //Backtracking function to check for all permutations of the given array
     void findPerms(string curr,vector<int>& a,string& ans){
         if(curr.size()==4){
             if(isOkay(curr))
+                //Updating maximum possible time
                 ans=max(ans,curr);
             return;
         }
@@ -25,6 +28,8 @@ public:
             curr+=to_string(temp);
             a.erase(a.begin()+i);
             findPerms(curr,a,ans);
+            
+            //BACKTRACK
             a.insert(a.begin()+i,temp);
             curr.pop_back();
         }
@@ -33,8 +38,12 @@ public:
     string largestTimeFromDigits(vector<int>& a) {
         string ans="";
         findPerms("",a,ans);
+        
+        //If empty, just return
         if(!ans.size())
             return ans;
+        
+        //Else add ':' to answer
         return ans.substr(0,2)+":"+ans.substr(2,2);
     }
 };
